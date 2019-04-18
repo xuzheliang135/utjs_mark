1d164ef5b1038eb6e3b7c538972f94b26a0e5144
xuzheliang135 <37364772+xuzheliang135@users.noreply.github.com>
2019-04-18 13:27:18 +0800

    优化了管理页面的显示： 1、创建时间中文化 2、加入了默认排序列 3、启用了列表页面编辑

--- app/admin_view_model.py
+++ app/admin_view_model.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import url_for, request, redirect
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
@@ -13,6 +15,7 @@ class BaseModelView(ModelView):
 
 class UserModelView(BaseModelView):
     column_list = ('create_time', 'username', 'name', 'team_id')
+    column_editable_list = ('name', 'team_id')
     form_columns = ('name', 'team_id')
     can_create = False
     column_labels = {
@@ -21,12 +24,18 @@ class UserModelView(BaseModelView):
         'name': '姓名',
         'team_id': '组号'
     }
+    column_default_sort = 'team_id'
     column_display_pk = True
+    column_formatters = {
+        'create_time': lambda view, context, model, name:
+        '' if not model.create_time else datetime.fromtimestamp(model.create_time).strftime('%Y-%m-%d %X')
+    }
 
 
 class VoteEventModelView(BaseModelView):
     can_delete = False
     column_list = ('create_time', 'event_id', 'lecture_team', 'vote_team', 'is_active')
+    column_editable_list = ('lecture_team', 'vote_team', 'is_active')
     form_columns = ('lecture_team', 'vote_team', 'is_active')
     column_labels = {
         'event_id': '事件号',
@@ -35,4 +44,9 @@ class VoteEventModelView(BaseModelView):
         'vote_team': '点评组号',
         'is_active': '是否为当前投票'
     }
+    column_default_sort = 'create_time'
     column_display_pk = True
+    column_formatters = {
+        'create_time': lambda view, context, model, name:
+        '' if not model.create_time else datetime.fromtimestamp(model.create_time).strftime('%Y-%m-%d %X')
+    }
